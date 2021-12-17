b0a89c93237093b8a865610f08cbcffce9cc3040
Nordii <aaronvey32@gmail.com>
2021-12-17 10:01:40 -0330

    now i need to make like 16 f**ing new lines

--- generate.bat
+++ generate.bat
@@ -2,4 +2,4 @@
 dir /w C:\Users
 set /p id="Type the folder that looks most like your username: "
 
-BatchSubstitute.bat "USERNAME" %id% C:\Users\%id%\Documents\GitHub\WSB-autosetup\
+BatchSubstitute.bat "USERNAME" %id% C:\Users\%id%\Documents\GitHub\WSB-autosetup
