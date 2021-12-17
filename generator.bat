edaeef39b7c3574172f507d01545c89874222762
Nordii <aaronvey32@gmail.com>
2021-12-17 13:00:12 -0330

    oh man

--- generator.bat
+++ generator.bat
@@ -1,8 +1,8 @@
-@echo on
-if %WSBGENPROG% neq 00 goto Restart else goto Main
+@echo off
+if %WSBGENPROG% gtr 00 goto Restart else goto Main
 
 :Main
-dir /w C:\Users
+dir C:\Users
 set /p WSBUSERNAME="Type the folder that corresponds to your username: "
 echo Do you want vGPU ENABLE or DISABLE?
 set /p WSBVGPU="vGPU: "
@@ -12,43 +12,40 @@ echo How much RAM (in MB) do you want to allow the Sandbox to consume?
 set /p WSBRAM="Maximum RAM in MB: "
 if exist temp goto SetVar else mkdir temp
 :SetVar
-del generate.bat
-echo @echo off>generate.bat
-echo set WSBGENPROG=03>generate.bat
-echo generator.bat>generate.bat
+call regenerate-generate
+echo generator.bat>> generate.bat
 set WSBGENPROG=04
 goto Restart
 :Breakoff1
-del generate.bat
-echo @echo off>generate.bat
-echo set WSBGENPROG=03>generate.bat
-echo generator.bat>generate.bat
+call regenerate-generate
+echo set WSBGENPROG=03>> generate.bat
+echo generator.bat>> generate.bat
 BatchSubstitute.bat "WSBUSERNAME" %WSBUSERNAME% ungenerated.wsb>temp\temp.wsb
 :Breakoff2
-del generate.bat
-echo @echo off>generate.bat
-echo set WSBGENPROG=02>generate.bat
-echo generator.bat>generate.bat
-BatchSubstitute.bat "WSBVGPU" %WSBVGPU% temp.wsb>temp\temp2.wsb
+call regenerate-generate
+echo set WSBGENPROG=02>> generate.bat
+echo generator.bat>> generate.bat
+cd temp
+BatchSubstitute.bat "WSBVGPU" %WSBVGPU% temp.wsb>temp2.wsb
 :Breakoff3
-del generate.bat
-echo @echo off> generate.bat
-echo set WSBGENPROG=01> generate.bat
-echo generator.bat> generate.bat
-BatchSubstitute.bat "WSBNET" %WSBNET% temp2.wsb>temp\temp3.wsb
+call regenerate-generate
+echo set WSBGENPROG=01>> generate.bat
+echo generator.bat>> generate.bat
+cd temp
+BatchSubstitute.bat "WSBNET" %WSBNET% temp2.wsb>temp3.wsb
 :Breakoff4
-del generate.bat
-echo @echo off> generate.bat
-echo set WSBGENPROG=05> generate.bat
-echo generator.bat> generate.bat
-BatchSubstitute.bat "WSBRAM" %WSBRAM% temp3.wsb>temp\temp4.wsb
+call regenerate-generate
+echo set WSBGENPROG=05>> generate.bat
+echo generator.bat>> generate.bat
+cd temp
+BatchSubstitute.bat "WSBRAM" %WSBRAM% temp3.wsb>temp4.wsb
 :CompleteCopy
 copy temp\temp4.wsb .\
 rmdir temp
 del generate.bat
-echo @echo off>generate.bat
-echo set WSBGENPROG=00>generate.bat
-echo generator.bat>generate.bat
+echo @echo off>> generate.bat
+echo set WSBGENPROG=00>> generate.bat
+echo generator.bat>> generate.bat
 if %WSBVGPU%=ENABLE set FNVGPU=vGPU- else set FNVGPU=
 if %WSBNET%=ENABLE set FNNET=Net- else set FNNET=
 rename temp4.wsb "Run-%FNVGPU%%FNNET%%WSBRAM%MB.wsb"
@@ -61,6 +58,7 @@ exit
 :Restart
 echo The script needs to restart %WSBGENPROG% more times to complete generating the WSB file.
 echo After the script exits, run generate.bat to continue
+echo 
 pause
 if %WSBGENPROG% equ 04 goto Breakoff1 else goto Check03
 :Check03
@@ -77,3 +75,11 @@ echo THE SCRIPT BROKE
 echo (this will need manual troubleshooting...)
 pause
 exit
+
+:regenerate-generate
+del generate.bat
+echo @echo off> generate.bat
+echo set WSBUSERNAME=%WSBUSERNAME%> generate.bat
+echo set WSBVGPU=%WSBVGPU%> generate.bat
+echo set WSBNET=%WSBNET%> generate.bat
+echo set WSBRAM=%WSBRAM%> generate.bat
