1acab0656919e9c4bac9fdf8a6f239b3cd7f562a
Nordii <aaronvey32@gmail.com>
2021-12-17 10:12:41 -0330

    Update generate.bat

--- generate.bat
+++ generate.bat
@@ -2,7 +2,7 @@
 dir /w C:\Users
 set /p id="Type the folder that looks most like your username: "
 cd 2GB
-..\BatchSubstitute.bat "USERNAME" %id% Run-2GB.wsb>Run-2GB.wsb
+..\BatchSubstitute.bat "USERNAME" %id% Run-2GB.wsb >> Run-2GB.wsb
 :: cd..
 :: cd 4GB
 :: ..\BatchSubstitute.bat "USERNAME" %id% *.wsb>*.wsb
