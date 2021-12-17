25a6cc035aca146824e0a673dd064e02a228a5c7
Nordii <aaronvey32@gmail.com>
2021-12-17 09:58:22 -0330

    more testing

--- generate.bat
+++ generate.bat
@@ -2,3 +2,4 @@
 dir /w C:\Users
 set /p id="Type the folder that looks most like your username: "
 
+echo `<HostFolder>C:\Users\%id%\Documents\GitHub\WSB-autosetup</HostFolder>\n   <SandboxFolder>C:\WSB-autosetup</SandboxFolder>\n   <ReadOnly>True</ReadOnly>\n  </MappedFolder>\n </MappedFolders>\n <LogonCommand>\n  <Command>C:\WSB-autosetup\Install.bat</Command>\n </LogonCommand>\n</Configuration>` >> C:\Users\%id%\Documents\GitHub\WSB-autosetup\6GB\test.wsb
