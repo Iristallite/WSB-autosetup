898265b7726da261d3a26c2f38640367410c3833
Nordii <aaronvey32@gmail.com>
2021-12-17 10:11:48 -0330

    testing

--- generate.bat
+++ generate.bat
@@ -2,16 +2,16 @@
 dir /w C:\Users
 set /p id="Type the folder that looks most like your username: "
 cd 2GB
-..\BatchSubstitute.bat "USERNAME" %id% *.wsb>*.wsb
-cd..
-cd 4GB
-..\BatchSubstitute.bat "USERNAME" %id% *.wsb>*.wsb
-cd..
-cd 6GB
-..\BatchSubstitute.bat "USERNAME" %id% *.wsb>*.wsb
-cd..
-cd 8GB
-..\BatchSubstitute.bat "USERNAME" %id% *.wsb>*.wsb
+..\BatchSubstitute.bat "USERNAME" %id% Run-2GB.wsb>Run-2GB.wsb
+:: cd..
+:: cd 4GB
+:: ..\BatchSubstitute.bat "USERNAME" %id% *.wsb>*.wsb
+:: cd..
+:: cd 6GB
+:: ..\BatchSubstitute.bat "USERNAME" %id% *.wsb>*.wsb
+:: cd..
+:: cd 8GB
+:: ..\BatchSubstitute.bat "USERNAME" %id% *.wsb>*.wsb
 echo WSB files generated succesfully.
 pause
 exit
