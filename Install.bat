dc609f96a788caf28afcb86cd9a7ad1995c47795
Nordii <aaronvey32@gmail.com>
2021-12-17 09:31:44 -0330

    reorganiuze

--- Install.bat
+++ Install.bat
@@ -1,15 +1,17 @@
 C:\WSB-autosetup\vbs\cpp.vbs
 :: Extract cpp folder
 C:\WSB-autosetup\cpp.exe -y -o"C:\cpp"
-C:\WSB-autosetup\vbs\cpp-finished.vbs
+
 :: Run VC++ x86 installers
 C:\cpp\2010x86.exe /norestart /passive
 C:\cpp\2012x86.exe /install /passive /norestart
 C:\cpp\2013x86.exe /install /passive /norestart
 C:\cpp\20152022x86.exe /install /passive /norestart
+C:\WSB-autosetup\vbs\cpp-x86-fin.vbs
 
 :: Run VC++ x64 installers
 C:\cpp\2010x64.exe /norestart /passive
 C:\cpp\2012x64.exe /install /passive /norestart
 C:\cpp\2013x64.exe /install /passive /norestart
 C:\cpp\20152022x64.exe /install /passive /norestart
+C:\WSB-autosetup\vbs\cpp-x64-fin.vbs
