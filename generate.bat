fdb9cb27b5b0708ce14cec918c97143e9c75261a
Nordii <aaronvey32@gmail.com>
2021-12-17 10:06:39 -0330

    operators

--- generate.bat
+++ generate.bat
@@ -1,8 +1,7 @@
 @echo off
 dir /w C:\Users
 set /p id="Type the folder that looks most like your username: "
-cd 2GB
-..\BatchSubstitute.bat "USERNAME" %id% *.wsb
+BatchSubstitute.bat "USERNAME" %id% >> C:\Users\%id%\Documents\GitHub\WSB-autosetup\*GB\*.wsb
 :: BatchSubstitute.bat "USERNAME" %id% C:\Users\%id%\Documents\GitHub\WSB-autosetup\2GB
 :: BatchSubstitute.bat "USERNAME" %id% C:\Users\%id%\Documents\GitHub\WSB-autosetup\2GB
 :: BatchSubstitute.bat "USERNAME" %id% C:\Users\%id%\Documents\GitHub\WSB-autosetup\2GB
